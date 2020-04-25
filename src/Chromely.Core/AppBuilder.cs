38361d8f072fd7a5c75c506a104662ebac5bc07a
elgonzo <6129198+elgonzo@users.noreply.github.com>
2020-04-25 11:42:48 +0200

    Update AppBuilder.cs
    EnsureIsDerivedType check more robust and with added interface/abstract class check for derivedType parameter

--- src/Chromely.Core/AppBuilder.cs
+++ src/Chromely.Core/AppBuilder.cs
@@ -141,14 +141,19 @@ namespace Chromely.Core
 
         private void EnsureIsDerivedType(Type baseType, Type derivedType)
         {
-            if (derivedType.FullName.Equals(baseType.FullName, StringComparison.OrdinalIgnoreCase))
+            if (baseType == derivedType)
             {
-                throw new Exception($"Type {derivedType.Name} must implement {baseType.Name}.");
+                throw new Exception($"Cannot specify the base type {baseType.Name} itself as generic type parameter.");
             }
 
             if (!baseType.IsAssignableFrom(derivedType))
             {
                 throw new Exception($"Type {derivedType.Name} must implement {baseType.Name}.");
+            }
+
+            if (derivedType.IsAbstract || derivedType.IsInterface)
+            {
+                throw new Exception($"Type {derivedType.Name} cannot be an interface or abstract class.");
             }
         }
     }
