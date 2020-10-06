df7423ad330724b6bb188840e7c8c0afe171048b
Stefano Moioli <smxdev4@gmail.com>
2020-10-06 22:44:32 +0200

    ChromelyBasicApp: make ConfigureCoreServices overridable

--- src/Chromely/ChromelyBasicApp.cs
+++ src/Chromely/ChromelyBasicApp.cs
@@ -15,7 +15,7 @@ namespace Chromely
     /// </summary>
     public class ChromelyBasicApp: ChromelyAppBase
     {
-        public sealed override void ConfigureCoreServices(ServiceCollection services)
+        public override void ConfigureCoreServices(ServiceCollection services)
         {
             base.ConfigureCoreServices(services);
 
