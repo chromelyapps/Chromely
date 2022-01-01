261e2a114a27293e05002548769927234be4a1f5
mattkol <softoille@gmail.com>
2022-01-01 14:37:09 -0500

    Upgrade to 5.2 issue #355 - added support for .NET Framework

--- src_5.2/Chromely.Core/IChromelySchemeHandler.cs
+++ src_5.2/Chromely.Core/IChromelySchemeHandler.cs
@@ -1,15 +1,14 @@
 ﻿// Copyright © 2017 Chromely Projects. All rights reserved.
 // Use of this source code is governed by MIT license that can be found in the LICENSE file.
 
-namespace Chromely.Core
+namespace Chromely.Core;
+
+public interface IChromelySchemeHandler
 {
-    public interface IChromelySchemeHandler
-    {
-        string Name { get; set; }
-        UrlScheme Scheme { get; set; }
-        object Handler { get; set; }
-        object HandlerFactory { get; set; }
-        bool IsCorsEnabled { get; set; }
-        bool IsSecure { get; set; }
-    }
+    string Name { get; set; }
+    UrlScheme Scheme { get; set; }
+    object Handler { get; set; }
+    object HandlerFactory { get; set; }
+    bool IsCorsEnabled { get; set; }
+    bool IsSecure { get; set; }
 }
